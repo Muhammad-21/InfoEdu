a9f9e241d2e3f6e734e640740516b6859192599b
Muhammad <muhammadmahmudov21@yandex.ru>
2021-05-08 16:39:52 +0300

    student page

---
+++ student.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Личный кабинет</title>
+</head>
+<body>
+    <?php require 'blocks/header.php'?>
+    <div data-course="courses">
+    <h4 style="margin-top:5%; color:Navy; margin-left:15%;">Все курсы</h4>
+    <p style="border: 5px solid Navy; padding: 40px; margin-left:15%;margin-right:15%;">
+        <a href="" style="color:black;" >Основы автоматизированных информационных технологий.</a>
+        <span style="color: Navy;">Тихомирова Д.В.</span>
+    </p>
+    <p style="border: 5px solid Navy; padding: 40px; margin-left:15%;margin-right:15%;">
+        <a href="" style="color:black;" >Безопасность жизнедеятельности</a>
+    </p>
+    </div>
+    
+    <?php require 'blocks/footer.php' ?>
+    <script src="./js/header.js"></script>
+</body>
+</html>
