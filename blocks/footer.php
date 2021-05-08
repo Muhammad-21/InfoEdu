e4016876887b541d57b76feac9ea7dab19810250
Muhammad <muhammadmahmudov21@yandex.ru>
2021-05-08 16:37:28 +0300

    blocks(header,footer ...)

---
+++ blocks/footer.php
@@ -0,0 +1,28 @@
+<footer class="container pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <small class="d-block mb-3 text-muted">© 2021</small>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Другие сервисы</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="https://home.mephi.ru/" target="_blank">Корпоративный портал</a></li>
+          <li><a class="link-secondary" href="https://pdf.mephi.ru/" target="_blank">Аттестационные работы</a></li>
+          <li><a class="link-secondary" href="https://library.mephi.ru/" target="_blank">Библиотека</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>О проекте</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="">Цель проекта</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Помощь и поддержка</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="">Обратная связь</a></li>
+          <li><a class="link-secondary" href="">Контактная информация</a></li>
+        </ul>
+      </div>
+    </div>
+</footer>
---
+++ blocks/header.php
@@ -0,0 +1,11 @@
+<div style="background-color: Navy ;">
+<div class="d-flex align-items-center flex-column flex-md-row p-3 px-md-4  shadow-sm">
+    <a class="p-2 text-dark" href="./student.php" style="text-decoration: none;"><span style="color: white; font-size: 25px; font-style:italic">InfoEdu</span></a>
+    <nav class="my-2 my-md-0 me-md-3" >
+    <a class="btn" style="color: white;" href="./student.php">Главная</a>
+    <div  data-id="rating" class="btn"  href="" style="color: white;">Рейтинг</div>
+    <div  data-id="course" class="btn"  href="" style="color: white;">Мои курсы</div>
+    <a class="btn" style="color: white; position:absolute; margin-left:80%; bottom:90%;" href="/">Выход</a>
+    </nav>
+</div>
+</div>
