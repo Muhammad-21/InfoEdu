3016bd50541c03406e0f92f822bfef1aac6c648c
Muhammad <muhammadmahmudov21@yandex.ru>
2021-09-29 17:23:17 +0300

    update:changing styles for users

--- users.php
+++ users.php
@@ -14,22 +14,40 @@ $user_id=$_SESSION["id_user"];
     <title>Пользователи</title>
 </head>
 <body>
-<div data-id="us"><br>
+  <br>
+  <h2 style="margin-left: 15%; color:navy;">Пользователи</h2><br>
+  <div style="border: 3px solid navy; margin-left:15%;margin-right:15%;"></div><br>
+<div data-id="us">
         <div>
         <?php 
         $mysql=new mysqli('localhost','root','','InfoEdu');
-        $results=$mysql->query("SELECT * From user");
+        $results=$mysql->query("SELECT * From user ORDER BY last_name");
         $us=$results->fetch_assoc();
-            do{ if($us['id_user']!=25 && $us['id_user']!=$user_id){?>
-              <div style="border: 1px solid #dfe4e9;padding:2%; color:navy; margin-left:10%;margin-right:10%;">
-                <img style="border-radius:100px; box-shadow:0 0 15px #666; width:60px;" src="<?php echo '../img/users/'.$us['photo_link']?>" alt="фотография профиля">
-                  <a href="../account/accounts.php?user_id=<?php echo $us['id_user'];?>" style="color: navy;"> <?php echo $us['last_name'].' '.$us['name'].' '.$us['middle_name'].'</a>'.' '.'<a class="btn-lg" style="color: white;background-color:navy; box-shadow:0 0 15px #666; border-radius:100px;" href="../mail/mail.php?user_id='.$us['id_user'].'">'.'&#9993</a>'.'<br>';}?>
-              </div>
+            do{ if($us['id_user']!=25 && $us['id_user']!=$user_id){
+              $id = $us['id_user'];
+              $co = $mysql->query("SELECT * FROM `teacher` WHERE `id_user` = '$id'");
+              $count = $co -> fetch_assoc();
+              if($count){
+                $type = 'преподаватель';
+              }
+        ?>
+              <div style="display: grid; grid-template-columns:70px 300px 50px;border: 1px solid #dfe4e9;padding:1%; color:navy; margin-left:15%;margin-right:15%;">
+                  <div>
+                    <img style="border-radius:100px; box-shadow:0 0 15px #666; width:60px;" src="<?php echo '../img/users/'.$us['photo_link']?>" alt="фотография профиля">
+                  </div>
+                  <div>
+                    <a href="../account/accounts.php?user_id=<?php echo $us['id_user'];?>" style="color: navy;"> <?php echo $us['last_name'].' '.$us['name'].' '.$us['middle_name']?></a><br>
+                    <div class="badge-secondary badge" style="font-size:10px;"><?php echo $type;$type='';?></div>
+                  </div>
+                  <a class="btn-lg" style="color: white;background-color:navy; box-shadow:0 0 15px #666; border-radius:100px;padding:30%;" href="../mail/mail.php?user_id=<?php echo $us['id_user'];?>">&#9993</a>
+              </div><br>
               <?php
+              }
             }
             while ($us=$results->fetch_assoc());
-        ?>
+            ?>
         </div>
     </div>
+  <div style="border: 3px solid navy; margin-left:15%;margin-right:15%;"></div><br>
 </body>
 </html>
